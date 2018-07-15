52e5266747e1867c746ffa2be25f65efe94846ac
Bonnie P Gill <bonniepgill@Bonnies-MacBook-Pro.local>
2018-07-15 14:41:03 -0400

    mvp

--- client/src/app/reducers/index.js
+++ client/src/app/reducers/index.js
@@ -4,13 +4,15 @@ import registrationReducer from '../../features/register/regReducer';
 import loginReducer from '../../features/login/loginReducer.js';
 import ormReducer from './ormReducer.js';
 import createSessionReducer from '../../features/trainer/createSession/createSessionReducer.js';
+import webRTCReducer from '../../features/webRTC/webRTCReducers.js';
 
 const rootReducer = combineReducers({
     router: routeReducer,
     orm: ormReducer,
     registration: registrationReducer,
     login: loginReducer,
-    create: createSessionReducer
+    create: createSessionReducer,
+    webrtc: webRTCReducer
 });
 
 export default rootReducer;
--- client/src/features/webRTC/webRTCPage.js
+++ client/src/features/webRTC/webRTCPage.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getStream } from '../../common/utils';
+
+
+const mapStateToProps = (state) => {
+  return {
+    ids: state.webrtc.data,
+    loading: state.webrtc.loading
+  }
+}
 
 class WebRTCPage extends Component {
+  constructor(props) {
+    super(props);
+    this.remoteVideoRef = React.createRef();
+  }
+
+  componentDidUpdate(prevProps) {
+  if (this.props.ids !== prevProps.ids) {
+    this.remoteVideoRef.current.srcObject = getStream(this.props.ids[0]);
+  }
+}
+
   render() {
-    const dummyStream = new MediaStream();
-    console.log(dummyStream)
-    dummyStream.addTrack({id:1});
-    console.log(dummyStream);
+    const { loading } = this.props
+    let node
+    if(loading){
+      node = <div> WE ARE WAITING </div>
+    } else {
+      node = <video autoPlay ref={this.remoteVideoRef}></video>
+    }
     return(
       <div className="flex flex-column flex-auto items-center justify-center mt5">
-        <div> VIDEO GOES HERE</div>
+        {node}
       </div>
     )
   }
 }
 
 
-export default WebRTCPage;
+export default connect(mapStateToProps)(WebRTCPage);
